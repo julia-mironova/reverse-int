da0f240bfce1303742fb0fe882870d5e9c35356c
Yuliya Mironova <rabkevich15@gmail.com>
2021-08-15 00:24:51 +0200

    feat: reverse integer done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let reverse = +Math.abs(n).toString().split("").reverse().join("");
+    return reverse;
+};
+/*console.log(reverse(51));*/
